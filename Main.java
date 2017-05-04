3d837eeb033acb208dcfd86eda3394817af0430e
Ayanle Hashi <ahashi2000@gmail.com>
2017-05-04 12:53:45 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,34 @@
+import java.util.*;
+
 public class Main {
     
+    public static ArrayList<Item> allItems = new ArrayList<Item>();
+
+    public static void initialize() {
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setScale(0,200);
+        StdDraw.setPenColor(255,0,0);
+    }
+    
+    public static void initializeItems(Player p) {
+        allItems.add(new TheMark(p,100,100));
+    }
+   
+    public static void main(String[] args) {
+        initialize();
+        Player player = new Player();
+        initializeItems(player);
+        
+        while (true) {
+            StdDraw.clear(StdDraw.BLACK);
+            player.update();
+            
+            for (Item item : allItems) {
+                if (!item.isPickedUp()) item.draw();
+                item.pickup(player.x,player.y);
+            }
+            
+            StdDraw.show();
+        }
+    }
 }
