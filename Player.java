d44fcf7423d4d2135cc3ef8f41615ab72d3bf5fe
Ayanle Hashi <ahashi2000@gmail.com>
2017-05-04 12:53:55 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,7 +2,7 @@ import java.awt.event.KeyEvent;
 import java.util.*;
 
 public class Player {
-    public static double x = 100;
+    public static double x = 50;
     public static double y = 100;
     public static double size = 7;
 
@@ -12,14 +12,17 @@ public class Player {
     public static double decel = 0.98;
 
     public static double speed = 1.0;
-    public static double shotSpeed = 1.0;
+    public static double shotSpeed = 0.2;
     public static double shotRate = 1.0;
-    public static double damage;
+    public static double range = 500;
+    public static double damage = 3;
     
-    public static double delay = shotRate * 10;
+    public static final double initialDelay = shotRate * 200;
+    public static double delay = initialDelay;
+    
+    public static ArrayList<String> items = new ArrayList<String>();
 
-    public static List<List<Double>> tears = new ArrayList<List<Double>>();
-    //x,y,xvel,yvel
+    public static List<List<Double>> tears = new ArrayList<List<Double>>(); //x,y,xvel,yvel,distance
 
     public static void move() {
         if (StdDraw.isKeyPressed(KeyEvent.VK_W) && StdDraw.isKeyPressed(KeyEvent.VK_D)) {
@@ -67,13 +70,48 @@ public class Player {
     }
 
     public static void shoot() {
-        if (StdDraw.isKeyPressed(KeyEvent.VK_UP)) {
+        if (StdDraw.isKeyPressed(KeyEvent.VK_UP) && delay < 0) {
             List<Double> temp = new ArrayList<Double>();
             temp.add(x);
             temp.add(y);
             temp.add(0.0);
-            temp.add(shotSpeed);
+            temp.add(shotSpeed + yVel);
+            temp.add(0.0);
             tears.add(temp);
+            delay = initialDelay;
+        }
+        
+        if (StdDraw.isKeyPressed(KeyEvent.VK_DOWN) && delay < 0) {
+            List<Double> temp = new ArrayList<Double>();
+            temp.add(x);
+            temp.add(y);
+            temp.add(0.0);
+            temp.add(-shotSpeed + yVel);
+            temp.add(0.0);
+            tears.add(temp);
+            delay = initialDelay;
+        }
+        
+        if (StdDraw.isKeyPressed(KeyEvent.VK_LEFT) && delay < 0) {
+            List<Double> temp = new ArrayList<Double>();
+            temp.add(x);
+            temp.add(y);
+            temp.add(-shotSpeed + xVel);
+            temp.add(0.0);
+            temp.add(0.0);
+            tears.add(temp);
+            delay = initialDelay;
+        }
+        
+        if (StdDraw.isKeyPressed(KeyEvent.VK_RIGHT) && delay < 0) {
+            List<Double> temp = new ArrayList<Double>();
+            temp.add(x);
+            temp.add(y);
+            temp.add(shotSpeed + xVel);
+            temp.add(0.0);
+            temp.add(0.0);
+            tears.add(temp);
+            delay = initialDelay;
         }
     }
 
@@ -114,18 +152,27 @@ public class Player {
     }
 
     public static void draw() {
-        StdDraw.setPenColor(245, 160, 120);
-        StdDraw.filledCircle(x, y, size);
         StdDraw.setPenColor(120,220,255);
         
         for (int i=0;i<tears.size();i++) {
-            StdDraw.filledCircle(tears.get(i).get(0),tears.get(i).get(1),2);
+            StdDraw.filledCircle(tears.get(i).get(0),tears.get(i).get(1),damage / 1.5);
+            
+            tears.get(i).set(0,tears.get(i).get(0) + tears.get(i).get(2));
+            tears.get(i).set(1,tears.get(i).get(1) + tears.get(i).get(3));
+            
+            tears.get(i).set(4,tears.get(i).get(4) + 1);
+            if (tears.get(i).get(4) >= range) tears.remove(i);
         }
+        
+        StdDraw.setPenColor(245, 160, 120);
+        StdDraw.filledCircle(x, y, size);
     }
 
     public static void update() {
         draw();
         move();
         shoot();
+        
+        delay--;
     }
 }
